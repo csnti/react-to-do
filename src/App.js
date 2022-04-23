79b40a63ca11994413ef96ecebe7b4489b149ae4
C S <chris@MacBook-Air.local>
2022-04-23 16:06:33 -0700

    Added setState  hooks, event handlers, and taskList filter

--- src/App.js
+++ src/App.js
@@ -4,15 +4,33 @@ import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 import { nanoid } from "nanoid";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('Active');
 
   function addTask(name) {
     const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
     setTasks([...tasks, newTask]);
   }
 
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+      if (id === task.id) {
+
+        return {...task, name: newName}
+      } 
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
 
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map(task => {
@@ -32,30 +50,38 @@ function App(props) {
     setTasks(remainingTasks);
   }
   
-  
-  const taskList = tasks.map(task => (
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map(task => (
     <Todo
-        id={task.id}
-        name={task.name}
-        completed={task.completed}
-        key={task.id}
-        toggleTaskCompleted={toggleTaskCompleted}
-        deleteTask={deleteTask}
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
     />
-  )); 
+  ));
 
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+ 
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
   
-
   return (
     <div className="todoapp stack-large">
       <h1>To-Do</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList }  
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 function Form(props) {
-  const [name, setName] = useState('Use hooks!')
+  const [name, setName] = useState('')
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -14,11 +14,6 @@ function Form(props) {
   }
   return (
     <form onSubmit={handleSubmit}>
-      <h2 className="label-wrapper">
-        <label htmlFor="new-todo-input" className="label__lg">
-          What needs to be done?
-        </label>
-      </h2>
       <input
         type="text"
         id="new-todo-input"
